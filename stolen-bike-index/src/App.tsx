37c3084c7939c5378652be5264b09cc18db107c7
Roman Grinovski <romagrin36@gmail.com>
2019-02-04 20:32:35 +0300

    Add basic routing

--- stolen-bike-index/src/App.tsx
+++ stolen-bike-index/src/App.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Index from './pages/index/Index';
+import Details from './pages/details/Details';
 
 const AppWrapper = styled.div``;
 
 const Header = styled.header`
-  background: url(img/illustration.png) center repeat;
+  background: url(/img/illustration.png) center repeat;
   background-size: contain;
   height: 200px;
   display: flex;
@@ -35,17 +38,22 @@ const Heading = styled.div`
 class App extends Component {
   render() {
     return (
-      <AppWrapper>
-        <Header>
-          <Logo src="img/police-logo.svg" />
-          <HeadingWrapper>
-            <Heading>
-              <h1>Police Department of Berlin</h1>
-              <h2>Stolen bikes</h2>
-            </Heading>
-          </HeadingWrapper>
-        </Header>
-      </AppWrapper>
+      <Router>
+        <AppWrapper>
+          <Header>
+            <Logo src="/img/police-logo.svg" />
+            <HeadingWrapper>
+              <Heading>
+                <h1>Police Department of Berlin</h1>
+                <h2>Stolen bikes</h2>
+              </Heading>
+            </HeadingWrapper>
+          </Header>
+
+          <Route exact path="/" component={Index} />
+          <Route path="/details/:id" component={Details} />
+        </AppWrapper>
+      </Router>
     );
   }
 }
