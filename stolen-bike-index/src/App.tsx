4a9e6fa55917d0970aabd1958d33821d1068be74
Roman Grinovski <romagrin36@gmail.com>
2019-02-04 19:29:29 +0300

    Add styled-components Add header Add logo

--- stolen-bike-index/src/App.tsx
+++ stolen-bike-index/src/App.tsx
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div``;
+
+const Header = styled.header`
+  background: url(img/illustration.png) center repeat;
+  background-size: contain;
+  height: 200px;
+  display: flex;
+  color: #fff;
+  padding: 2%;
+
+  h1,
+  h2 {
+    margin: 0;
+  }
+`;
+
+const Logo = styled.img`
+  width: 60px;
+  height: 60px;
+`;
+
+const HeadingWrapper = styled.div`
+  display: inline-block;
+  margin-left: 15px;
+`;
+
+const Heading = styled.div`
+  background: #000;
+  opacity: 0.9;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppWrapper>
+        <Header>
+          <Logo src="img/police-logo.svg" />
+          <HeadingWrapper>
+            <Heading>
+              <h1>Police Department of Berlin</h1>
+              <h2>Stolen bikes</h2>
+            </Heading>
+          </HeadingWrapper>
+        </Header>
+      </AppWrapper>
     );
   }
 }
