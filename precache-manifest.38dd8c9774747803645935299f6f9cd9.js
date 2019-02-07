c6acec5e9cf26760b58efc7cef1b28fe713a789d
Roman Grinovski <romagrin36@gmail.com>
2019-02-07 05:08:27 +0300

    Updates

---
+++ precache-manifest.38dd8c9774747803645935299f6f9cd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c258f90bd0c65096878d",
+    "url": "/coding-challenge-frontend-react/static/js/runtime~main.c258f90b.js"
+  },
+  {
+    "revision": "70646e7120c31e12d078",
+    "url": "/coding-challenge-frontend-react/static/js/main.70646e71.chunk.js"
+  },
+  {
+    "revision": "2cd079927bbdc364a569",
+    "url": "/coding-challenge-frontend-react/static/js/1.2cd07992.chunk.js"
+  },
+  {
+    "revision": "70646e7120c31e12d078",
+    "url": "/coding-challenge-frontend-react/static/css/main.fc321640.chunk.css"
+  },
+  {
+    "revision": "c897b0f6043f569968f4d4dc391ced94",
+    "url": "/coding-challenge-frontend-react/index.html"
+  }
+];
